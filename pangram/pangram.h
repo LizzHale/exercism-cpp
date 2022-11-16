74f27f7b3cb1ce3ecacb54af5cf9518032b839c7
Lizz Hale <lizzhale@github.com>
2022-11-16 12:06:06 -0700

    Simple green

--- pangram/pangram.h
+++ pangram/pangram.h
@@ -1,7 +1,12 @@
 #if !defined(PANGRAM_H)
 #define PANGRAM_H
 
+#include <string>
+
 namespace pangram {
+
+    bool is_pangram(std::string sentence);
+    bool is_in_sentence(char character, std::string sentence);
 
 }  // namespace pangram
 
